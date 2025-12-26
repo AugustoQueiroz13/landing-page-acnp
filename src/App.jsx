0b59853aa7265c5f941940ee10bb6cca53c88935
Augusto Queiroz <augusto_queiroz@id.uff.br>
2025-12-26 07:22:32 -0300

    Correcao Final: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,28 @@
 import React from 'react';
-// CORREÇÃO 1: Importamos direto o BrowserRouter sem mudar o nome
+// AQUI ESTAVA O ERRO: Importamos do jeito certo agora
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Importação das Páginas
 import LandingPage from './LandingPage';
 import Login from './Login';
 import Dashboard from './Dashboard';
-import ValidarCertificado from './ValidarCertificado';
 import ConfirmationPage from './ConfirmationPage';
 import AdminDashboard from './AdminDashboard';
+import ValidarCertificado from './ValidarCertificado';
 
 export default function App() {
     return (
-        // CORREÇÃO 2: Removemos o <Router> externo duplicado.
-        // Mantemos apenas ESTE BrowserRouter com o basename correto.
+        // O basename garante que o site entenda que roda em /educacional
         <BrowserRouter basename="/educacional">
             <Routes>
-                {/* Rota Raiz: Mostra o Site de Vendas */}
                 <Route path="/" element={<LandingPage />} />
-
-                {/* Rota para Validar Codigo do Certificado */}
-                <Route path="/validar" element={<ValidarCertificado />} />
-
-                {/* Rota de Login */}
                 <Route path="/login" element={<Login />} />
-
-                {/* Rota do Aluno (Protegida) */}
                 <Route path="/dashboard" element={<Dashboard />} />
-
-                {/* Rota de Confirmação de Matrícula (Pós-Formulário) */}
-                <Route path="/confirmacao" element={<ConfirmationPage />} />
-
-                {/* Rota Admin */}
                 <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/confirmacao" element={<ConfirmationPage />} />
+                {/* <Route path="/validar" element={<ValidarCertificado />} /> */}
 
-                {/* Rota Curinga: Se digitar algo errado, volta pra Home */}
+                {/* Rota para pegar qualquer link errado e voltar para o inicio */}
                 <Route path="*" element={<LandingPage />} />
             </Routes>
         </BrowserRouter>
